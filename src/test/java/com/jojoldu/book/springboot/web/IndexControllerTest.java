01a64904d69aa3f8606f93ae53903d4a9078a5c6
shinru <shgytjq@gmail.com>
2020-05-17 22:29:06 +0900

    IndexControllerTest 글등록페이지_로딩, 글수정페이지_로딩 테스트 추가

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -14,6 +16,8 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
@@ -48,5 +52,36 @@ public class IndexControllerTest {
         assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
     }
 
+    @Test
+    @WithMockUser(roles = "USER")
+    public void 글등록페이지_로딩() throws Exception {
+        // when
+        String content = mvc.perform(get("/posts/save"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        // then
+        assertThat(content).contains("게시글 등록");
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void 글수정페이지_로딩() throws Exception {
+        // given
+        Posts savedPosts = postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+        Long id = savedPosts.getId();
+
+        // when
+        String content = mvc.perform(get("/posts/update/" + id))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        // then
+        assertThat(content).contains("게시글 수정");
+    }
 
 }
